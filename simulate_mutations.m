87bf0df32d04b2687d2c080915a799b64fc2e218
Sohit Miglani <sohitmiglani@gmail.com>
2022-09-20 00:29:24 -0400

    Create simulate_mutations.m

---
+++ simulate_mutations.m
@@ -0,0 +1,12 @@
+function [H] = simulate_mutations(C,N,p)
+	for i = 1:C
+		m = 0;
+		for j = 1:N
+    			out = rand(1,1);
+			if out < p
+				m = m +1;
+            end
+		end
+		H(i) = m;
+	end
+end
