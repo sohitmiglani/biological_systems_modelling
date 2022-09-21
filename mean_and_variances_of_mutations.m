14d62b44a8d83b5c4e49bce85ac884f3ace72584
Sohit Miglani <sohitmiglani@gmail.com>
2022-09-21 01:27:38 -0400

    Update mean_and_variances_of_mutations.m

--- mean_and_variances_of_mutations.m
+++ mean_and_variances_of_mutations.m
@@ -23,3 +23,28 @@ X = 2000:2700;
 Y = X*264756.819;
 plot(X,Y);
 hold off
+
+
+## If we remove the top 0.1% of the outliers (Part D), we get better estimates: 
+
+function [means, vars] = mutation_mean_and_var(C,N,p,G)
+    for sim = 1:10
+        H = [];
+        for i = 1:C
+            m = 0;
+            for g = 1:G
+		        m = 2*m + poissrnd((N*2^(g-1)-m)*p);
+            end
+            H(i) = m;
+        end
+        H_sorted = sort(H);
+        H_sorted = H_sorted(1:19980);
+        means(sim) = mean(H_sorted);
+        vars(sim) = var(H_sorted);
+    end
+end
+
+[means, vars] = mutation_mean_and_var(C,N,p,G);
+
+mean(means);
+mean(vars);
