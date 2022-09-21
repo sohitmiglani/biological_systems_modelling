2234b7d6adca03f49f699445d7605493574ada8f
Sohit Miglani <sohitmiglani@gmail.com>
2022-09-20 23:09:21 -0400

    Update simulate_mutations_over_generations.m

--- simulate_mutations_over_generations.m
+++ simulate_mutations_over_generations.m
@@ -2,7 +2,7 @@ function [H] = simulate_mutations_over_generations(C,N,p,G)
     for i = 1:C
         m = 0;
         for g = 1:G
-		      m = 2*m + poissrnd((N*2^(g-1)-m)*p);
+		m = 2*m + poissrnd((N*2^(g-1)-m)*p);
         end
         H(i) = m;
     end
